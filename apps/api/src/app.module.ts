6968774b00b0d60f8cac46c698860788bc19780f
Ngo Quoc Khiem <email_github_cua_ban@example.com>
2026-04-30 09:51:35 +0700

    refactor: unify apps structure to web and api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { FarmsModule } from './farms/farms.module';
+import { AiModule } from './ai/ai.module';
+import { IotModule } from './iot/iot.module';
+import { MarketplaceModule } from './marketplace/marketplace.module';
+
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    FarmsModule,
+    AiModule,
+    IotModule,
+    MarketplaceModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
